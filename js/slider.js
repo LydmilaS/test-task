6cb2d374f7de98ad51dfcd0ccfaaf1e1e7f142a6
LydmilaS <lydmilazoloto@gmail.com>
2016-05-10 16:58:23 +0300

    change href=#, create variable.scss, mobile version done, delete bootstrap

--- js/slider.js
+++ js/slider.js
@@ -3,9 +3,14 @@
 
     // Создаём настройки по-умолчанию, расширяя их с помощью параметров, которые были переданы
     var settings = $.extend( {
+    		wrapper: '.slider';
      		duration: 300,
-            slidesContainer: '.slider',
-            outsideMargin: 200
+            slidesContainer: '.slider-item',
+            outsideMargin: 200;
+            previous: '.prev';
+            next: '.next';
+            speed: 500,
+			easing: "linear"
     }, options);
 
     return this.each(function() {        
