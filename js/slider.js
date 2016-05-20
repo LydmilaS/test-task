259d764872048015f166c766feb2a6995d9301ac
LydmilaS <lydmilazoloto@gmail.com>
2016-05-20 12:01:26 +0300

    first version

--- js/slider.js
+++ js/slider.js
@@ -4,8 +4,6 @@
     
     var Slider = function(options, wrapper) {
       $.extend(this.options, options);
-      this.$wrap = $(wrapper);
-      this.$slides = this.$wrap.find('.slider-item').get();
       this.leftClickAction();
       this.rightClickAction();
     };
@@ -14,30 +12,53 @@
     Slider.prototype.options = {
       duration: 400,
       slidesContainer: '.slider',
+      sliderCount: '.slider-item',
       leftArrowNav: '.prev',
       rightArrowNav: '.next'
     };
 
-    Slider.prototype.leftClickAction = function(event) {
-     var width = $(window).width();
-     var i = $(".slider-item").size();
-       $(this.$slides).hide();
-       $(".slider-item.active").show();
-        $(this.options.leftArrowNav).one('click', function() {
-          var n = 1;
-          if (n < i) {
-            $(".slider-item").animate({right: width});
-            n++;
-          } else {
-            n = 1;
-          }
+    Slider.prototype.leftClickAction = function() {
+      var currentIndex = 0, 
+        width = $(window).width(), // width device
+        items = $(this.options.sliderCount),
+        itemsSize = items.size(), // 4
+        item = $(this.options.sliderCount).eq(currentIndex), 
+        thatDuration = $(this.options.duration);
+
+        $(this.options.leftArrowNav).click( function() {
+
+          currentIndex -=1;
+
+          if (currentIndex < 0) {
+              currentIndex = itemsSize -1;
+            }
+
+            $(item).animate({right: width}, {duration: thatDuration}); 
+            var item = $('.slider-item').eq(currentIndex);
+              items.hide();
+              item.css('display','inline-block');      
         }); 
     };
 
     Slider.prototype.rightClickAction = function() {
-      var width = $(window).width();
-      $(this.options.rightArrowNav).one('click', function() {
-        $(".slider-item").animate({left: width});
+      var currentIndex = 0, 
+        width = $(window).width(), // width device
+        items = $(this.options.sliderCount),
+        itemsSize = items.size(), // 4
+        item = $(this.options.sliderCount).eq(currentIndex);  
+
+      $(this.options.rightArrowNav).click(function() {
+
+        currentIndex += 1;
+          
+        if (currentIndex > itemsSize - 1) {
+          currentIndex = 0;
+        }
+
+        $(item).animate({left: width}); 
+          var item = $('.slider-item').eq(currentIndex);
+            items.hide();
+            item.css('display','inline-block');
       });     
     };
 
